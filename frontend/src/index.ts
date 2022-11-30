a143cf2a127a696a2d05a61ae493e092b0e71cac
yaroslavsf <yaroslav1598753@gmail.com>
2022-11-30 11:02:12 +0100

    Update directories names

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,11 +1,11 @@
-// export {}
-export {AuthPage } from './components/utilities/auth/AuthPage';
-export { Login } from './components/utilities/auth/Login';
-export { Registration } from './components/utilities/auth/Registration';
-export { NotFound } from './components/utilities/NotFound';
-export { AddRoom } from './components/utilities/user/AddRoom';
-export { BookRoom } from './components/utilities/user/BookRoom';
-export { Catalog } from './components/utilities/user/Catalog';
-export { HomePage } from './components/utilities/user/HomePage';
-export { Profile } from './components/utilities/user/Profile';
-export { RoomInfo } from './components/utilities/user/RoomInfo';
+export {}
+export {AuthPage } from './components/pages/auth/AuthPage';
+export { Login } from './components/pages/auth/Login';
+export { Registration } from './components/pages/auth/Registration';
+export { NotFound } from './components/pages/NotFound';
+export { AddRoom } from './components/pages/user/AddRoom';
+export { BookRoom } from './components/pages/user/BookRoom';
+export { Catalog } from './components/pages/user/Catalog';
+export { HomePage } from './components/pages/user/HomePage';
+export { Profile } from './components/pages/user/Profile';
+export { RoomInfo } from './components/pages/user/RoomInfo';
